3c14009dfb6d7c5cf4e5ebfc4d772127c7e30ef6
dani <danielme_com@yahoo.com>
2023-11-10 20:43:34 +0100

    auditing

---
+++ src/main/java/com/danielme/springdatajpa/model/entity/audit/User.java
@@ -0,0 +1,47 @@
+package com.danielme.springdatajpa.model.entity.audit;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class User //extends Auditable //HIERARCHY
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    //private Audit audit = new Audit(); //COMPOSITION
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+}
+
+
